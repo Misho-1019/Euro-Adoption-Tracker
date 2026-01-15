9361c9c500bc5aede3f2d49fd66f2daf4e0e0f2b
Misho1019 <mikailtorres99@gmail.com>
2026-01-15 20:07:30 +0200

    Create exchange rate repository with current rate lookup

---
+++ server/src/repositories/exchangeRate.js
@@ -0,0 +1,18 @@
+import prisma from "../prisma";
+
+export async function getCurrentRate(asOf = new Date()) {
+    const now = asOf instanceof Date ? asOf : new Date();
+
+    const rateRecord = await prisma.exchangeRate.findFirst({
+        where: { validFrom: { lte: now } },
+        orderBy: [{ validFrom: 'desc'}, { createdAt: 'desc' }, { id: 'desc' }]
+    })
+
+    if (!rateRecord) {
+        const err = new Error('EXCHANGE_RATE_NOT_FOUND')
+        err.code = 'EXCHANGE_RATE_NOT_FOUND'
+        throw err;
+    }
+
+    return rateRecord;
+}
