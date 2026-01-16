1bc267a53c3807c922705d0df223d40034f6d7e1
Misho1019 <mikailtorres99@gmail.com>
2026-01-16 03:11:16 +0200

    Add product price repository with history support

---
+++ server/src/repositories/productPriceRepository.js
@@ -0,0 +1,47 @@
+import prisma from "../prisma";
+
+export async function createPrice(data) {
+    return await prisma.productPrice.create({
+        data: {
+            productId: data.productId,
+            priceBgn: data.priceBgn,
+            rateBgnPerEur: data.rateBgnPerEur,
+            priceEurExact: data.priceEurExact,
+            priceEurDisplay: data.priceEurDisplay,
+            roundingPolicy: data.roundingPolicy,
+            effectiveFrom: data.effectiveFrom,
+        }
+    })
+}
+
+export async function getCurrentPrice(productId, asOf = new Date()) {
+    let asOfMoment;
+
+    if (asOf instanceof Date) {
+        asOfMoment = asOf;
+    } else {
+        asOfMoment = new Date(asOf)
+
+        if (Number.isNaN(asOfMoment.getTime())) {
+            throw new Error('Invalid asOf date')
+        }
+    }
+
+    const priceRecord = await prisma.productPrice.findFirst({
+        where: { productId, effectiveFrom: { lte: asOfMoment } },
+        orderBy: [{ effectiveFrom: 'desc' }, { createdAt: 'desc' }, { id: 'desc' }]
+    })
+
+    if (!priceRecord) {
+        return null;
+    }
+
+    return priceRecord;
+}
+
+export async function getPriceHistory(productId) {
+    return await prisma.productPrice.findMany({
+        where: { productId },
+        orderBy: [{ effectiveFrom: 'desc' }, { createdAt: 'desc' }, { id: 'desc' }]
+    })
+}
