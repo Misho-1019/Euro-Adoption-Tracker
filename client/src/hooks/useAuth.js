c3b715bc7bb2df9753827fdb6badcaac334e22fd
Misho1019 <mikailtorres99@gmail.com>
2026-01-27 04:29:25 +0200

    Create useAuth custom hook

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import requester from "../utils/requester";
+
+export default function useAuth() {
+    const authData = useContext(UserContext)
+
+    const requestWrapper = (method, url, data, options = {}) => {
+        const optionsWrapper = {
+            ...options,
+            headers: {
+                'Content-Type': 'application/json',
+                ...options.headers
+            }
+        }
+
+        return requester(method, url, data, optionsWrapper)
+    }
+
+    return {
+        ...authData,
+        request: {
+            get: requestWrapper.bind(null, 'GET'),
+            post: requestWrapper.bind(null, 'POST'),
+            put: requestWrapper.bind(null, 'PUT'),
+            delete: requestWrapper.bind(null, 'DELETE'),
+        }
+    }
+}
